7ee9d72a8a926b5a0cd29b1ab442c4cde0177b73
Emin Siljak <esiljak1@etf.unsa.ba>
2022-09-12 19:22:08 +0200

    Added safety checks to constructor in ReceiptItem abstract class

--- src/com/esiljak/models/ReceiptItem.java
+++ src/com/esiljak/models/ReceiptItem.java
@@ -1,17 +1,39 @@
 package com.esiljak.models;
 
+import com.esiljak.exceptions.IllegalItemNameException;
+import com.esiljak.exceptions.IllegalPriceException;
+import com.esiljak.exceptions.IllegalQuantityException;
+
 public abstract class ReceiptItem {
     private String name;
     private float price;
     private int quantity;
 
-    public ReceiptItem(String name, float price) {
+    private void checkItemName(String itemName) throws IllegalItemNameException {
+        if(itemName == null || itemName.trim().isEmpty())
+            throw new IllegalItemNameException("Item name cannot be null or empty");
+    }
+
+    private void checkPrice(float price) throws IllegalPriceException {
+        if(price < 0)
+            throw new IllegalPriceException("Price cannot be negative");
+    }
+
+    private void checkQuantity(int quantity) throws IllegalQuantityException {
+        if(quantity <= 0)
+            throw new IllegalQuantityException("Quantity must be positive");
+    }
+
+    public ReceiptItem(String name, float price) throws IllegalItemNameException, IllegalPriceException {
+        checkItemName(name);
+        checkPrice(price);
         this.name = name;
         this.price = price;
     }
 
-    public ReceiptItem(String name, float price, int quantity) {
+    public ReceiptItem(String name, float price, int quantity) throws IllegalItemNameException, IllegalPriceException, IllegalQuantityException {
         this(name, price);
+        checkQuantity(quantity);
         this.quantity = quantity;
     }
 
@@ -19,7 +41,8 @@ public abstract class ReceiptItem {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) throws IllegalItemNameException {
+        checkItemName(name);
         this.name = name;
     }
 
@@ -27,7 +50,8 @@ public abstract class ReceiptItem {
         return price;
     }
 
-    public void setPrice(float price) {
+    public void setPrice(float price) throws IllegalPriceException {
+        checkPrice(price);
         this.price = price;
     }
 
@@ -35,7 +59,8 @@ public abstract class ReceiptItem {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
+    public void setQuantity(int quantity) throws IllegalQuantityException {
+        checkQuantity(quantity);
         this.quantity = quantity;
     }
 
