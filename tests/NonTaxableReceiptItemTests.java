e38c1e4389f231e73ad321164f67d732ee78e1c4
Emin Siljak <esiljak1@etf.unsa.ba>
2022-09-12 20:13:19 +0200

    Created tests for NonTaxableReceiptItem class

---
+++ tests/NonTaxableReceiptItemTests.java
@@ -0,0 +1,100 @@
+import com.esiljak.exceptions.IllegalItemNameException;
+import com.esiljak.exceptions.IllegalPriceException;
+import com.esiljak.exceptions.IllegalQuantityException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NonTaxableReceiptItemTests {
+    private final String ITEM_NAME = "non taxable";
+    private final float PRICE = 5f;
+    private final int QUANTITY = 3;
+
+    @Test
+    void basicCreateTest(){
+        NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE, QUANTITY);
+
+        assertEquals(ITEM_NAME, item.getName(), "Item name not set correctly through the constructor");
+        assertEquals(PRICE, item.getPrice(), "Item price not set correctly through the constructor");
+        assertEquals(QUANTITY, item.getQuantity(), "Item quantity not set correctly through the constructor");
+    }
+
+    @Test
+    void noQuantityPassedTest(){
+        NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE);
+        assertEquals(1, item.getQuantity(), "Quantity must be 1 if it's not passed in the constructor");
+    }
+
+    @Test
+    void setterTest(){
+        NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE, QUANTITY);
+
+        item.setName("new name");
+        assertEquals("new name", item.getName(), "Name not set properly through setter");
+
+        item.setPrice(PRICE * 2);
+        assertEquals(PRICE * 2, item.getPrice(), "Price not set properly through setter");
+
+        item.setQuantity(QUANTITY - 2);
+        assertEquals(QUANTITY - 2, item.getQuantity(), "Quantity not set properly through setter");
+    }
+
+    @Test
+    void nonPositiveQuantity(){
+        assertThrows(IllegalQuantityException.class, () -> {
+           new NonTaxableReceiptItem(ITEM_NAME, PRICE, 0);
+        }, "Quantity cannot be negative or zero - constructor");
+
+        assertThrows(IllegalQuantityException.class, () -> {
+            NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE, QUANTITY);
+            item.setQuantity(0);
+        }, "Quantity cannot be negative or zero - setter");
+    }
+
+    @Test
+    void negativePriceTest(){
+        assertThrows(IllegalPriceException.class, () -> {
+            new NonTaxableReceiptItem(ITEM_NAME, -1f);
+        }, "Price cannot be negative - constructor");
+
+        assertThrows(IllegalPriceException.class, () -> {
+            NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE);
+            item.setPrice(-1f);
+        }, "Price cannot be negative - setter");
+
+        assertDoesNotThrow(() -> {
+            NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, 0f);
+            item.setPrice(0f);
+        }, "Price can be zero - constructor and setter");
+    }
+
+    @Test
+    void emptyNameTest(){
+        assertThrows(IllegalItemNameException.class, () -> {
+           new NonTaxableReceiptItem("", PRICE);
+        }, "Name cannot be empty - constructor");
+
+        assertThrows(IllegalItemNameException.class, () -> {
+            NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE);
+            item.setName("  ");
+        }, "Name cannot be empty - setter");
+
+        assertThrows(IllegalItemNameException.class, () -> {
+            new NonTaxableReceiptItem(null, PRICE);
+        }, "Name cannot be null - constructor");
+
+        assertThrows(IllegalItemNameException.class, () -> {
+            NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE);
+            item.setName(null);
+        }, "Name cannot be null - setter");
+    }
+
+    @Test
+    void calculateTaxTest(){
+        NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE, QUANTITY);
+        assertEquals(0, item.calculateTax(), "For non taxable item tax has to be zero");
+
+        item.setQuantity(1);
+        assertEquals(0, item.calculateTax(), "For non taxable item tax has to be zero");
+    }
+}
