4190d0b3dfd4b36beb117e8115ddb54eacdbcdc8
Emin Siljak <esiljak1@etf.unsa.ba>
2022-09-12 20:59:59 +0200

    Created tests for ImportedBasicReceiptItem class

---
+++ tests/ImportedBasicReceiptItemTests.java
@@ -0,0 +1,50 @@
+import com.esiljak.helpers.NumberHelper;
+import com.esiljak.models.BasicReceiptItem;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ImportedBasicReceiptItemTests {
+    private final String ITEM_NAME = "item name";
+    private final float PRICE = 10;
+    private final int QUANTITY = 2;
+
+    private ImportedBasicReceiptItem item;
+
+    @BeforeEach
+    void initialize(){
+        item = new ImportedBasicReceiptItem(ITEM_NAME, PRICE, QUANTITY);
+    }
+
+    @Test
+    void checkSuperclassTest(){
+        assertTrue(item instanceof BasicReceiptItem, "ImportedBasicReceiptItem class has to extend BasicReceiptItem class");
+    }
+
+    @Test
+    void constructorTest(){
+        assertEquals(ITEM_NAME, item.getName(), "Name not set correctly through the constructor");
+        assertEquals(PRICE, item.getPrice(), "Price not set correctly through the constructor");
+        assertEquals(QUANTITY, item.getQuantity(), "Quantity not set correctly through the constructor");
+    }
+
+    @Test
+    void setterTest(){
+        item.setName(ITEM_NAME + " new");
+        assertEquals(ITEM_NAME + " new", item.getName(), "Name not set correctly through the setter");
+
+        item.setPrice(PRICE - 2);
+        assertEquals(PRICE - 2, item.getPrice(), "Price not set correctly through the setter");
+
+        item.setQuantity(QUANTITY * 2);
+        assertEquals(QUANTITY * 2, item.getQuantity(), "Quantity not set correctly through the setter");
+    }
+
+    @Test
+    void calculateTaxTest(){
+        float tax = 15 * (PRICE * QUANTITY) / 100;
+        assertEquals(NumberHelper.roundUpTax(tax), item.calculateTax(), "Tax not calculated by the correct formula");
+    }
+}
