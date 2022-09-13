170fc25de2a7c55e18a54efcb21bd487825dfc76
Emin Siljak <esiljak1@etf.unsa.ba>
2022-09-13 12:21:49 +0200

    Refactored NonTaxableReceiptItemTests

--- tests/NonTaxableReceiptItemTests.java
+++ tests/NonTaxableReceiptItemTests.java
@@ -2,6 +2,7 @@ import com.esiljak.exceptions.IllegalItemNameException;
 import com.esiljak.exceptions.IllegalPriceException;
 import com.esiljak.exceptions.IllegalQuantityException;
 import com.esiljak.models.NonTaxableReceiptItem;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,11 +11,15 @@ public class NonTaxableReceiptItemTests {
     private final String ITEM_NAME = "non taxable";
     private final float PRICE = 5f;
     private final int QUANTITY = 3;
+    private NonTaxableReceiptItem item;
+
+    @BeforeEach
+    void initialize() throws Exception{
+        item = new NonTaxableReceiptItem(ITEM_NAME, PRICE, QUANTITY);
+    }
 
     @Test
-    void basicCreateTest() throws Exception {
-        NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE, QUANTITY);
-
+    void basicCreateTest(){
         assertEquals(ITEM_NAME, item.getName(), "Item name not set correctly through the constructor");
         assertEquals(PRICE, item.getPrice(), "Item price not set correctly through the constructor");
         assertEquals(QUANTITY, item.getQuantity(), "Item quantity not set correctly through the constructor");
@@ -28,8 +33,6 @@ public class NonTaxableReceiptItemTests {
 
     @Test
     void setterTest() throws Exception{
-        NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE, QUANTITY);
-
         item.setName("new name");
         assertEquals("new name", item.getName(), "Name not set properly through setter");
 
@@ -43,56 +46,51 @@ public class NonTaxableReceiptItemTests {
     @Test
     void nonPositiveQuantity() {
         assertThrows(IllegalQuantityException.class, () -> {
-           new NonTaxableReceiptItem(ITEM_NAME, PRICE, 0);
-        }, "Quantity cannot be negative or zero - constructor");
-
-        assertThrows(IllegalQuantityException.class, () -> {
-            NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE, QUANTITY);
             item.setQuantity(0);
         }, "Quantity cannot be negative or zero - setter");
+
+        assertThrows(IllegalQuantityException.class, () -> {
+           new NonTaxableReceiptItem(ITEM_NAME, PRICE, 0);
+        }, "Quantity cannot be negative or zero - constructor");
     }
 
     @Test
     void negativePriceTest(){
         assertThrows(IllegalPriceException.class, () -> {
-            new NonTaxableReceiptItem(ITEM_NAME, -1f);
-        }, "Price cannot be negative - constructor");
-
-        assertThrows(IllegalPriceException.class, () -> {
-            NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE);
             item.setPrice(-1f);
         }, "Price cannot be negative - setter");
 
+        assertThrows(IllegalPriceException.class, () -> {
+            new NonTaxableReceiptItem(ITEM_NAME, -1f);
+        }, "Price cannot be negative - constructor");
+
         assertDoesNotThrow(() -> {
-            NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, 0f);
-            item.setPrice(0f);
+            NonTaxableReceiptItem testItem = new NonTaxableReceiptItem(ITEM_NAME, 0f);
+            testItem.setPrice(0f);
         }, "Price can be zero - constructor and setter");
     }
 
     @Test
     void emptyNameTest(){
         assertThrows(IllegalItemNameException.class, () -> {
-           new NonTaxableReceiptItem("", PRICE);
-        }, "Name cannot be empty - constructor");
-
-        assertThrows(IllegalItemNameException.class, () -> {
-            NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE);
             item.setName("  ");
         }, "Name cannot be empty - setter");
 
         assertThrows(IllegalItemNameException.class, () -> {
-            new NonTaxableReceiptItem(null, PRICE);
-        }, "Name cannot be null - constructor");
-
-        assertThrows(IllegalItemNameException.class, () -> {
-            NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE);
             item.setName(null);
         }, "Name cannot be null - setter");
+
+        assertThrows(IllegalItemNameException.class, () -> {
+           new NonTaxableReceiptItem("", PRICE);
+        }, "Name cannot be empty - constructor");
+
+        assertThrows(IllegalItemNameException.class, () -> {
+            new NonTaxableReceiptItem(null, PRICE);
+        }, "Name cannot be null - constructor");
     }
 
     @Test
     void calculateTaxTest() throws Exception{
-        NonTaxableReceiptItem item = new NonTaxableReceiptItem(ITEM_NAME, PRICE, QUANTITY);
         assertEquals(0, item.calculateTax(), "For non taxable item tax has to be zero");
 
         item.setQuantity(1);
