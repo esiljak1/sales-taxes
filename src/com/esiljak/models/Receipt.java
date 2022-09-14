770a4f7254bd9e038219a7ba4ce69c752e83a62a
Emin Siljak <esiljak1@etf.unsa.ba>
2022-09-14 20:48:13 +0200

    Implemented toString for Receipt class

--- src/com/esiljak/models/Receipt.java
+++ src/com/esiljak/models/Receipt.java
@@ -5,6 +5,7 @@ import com.esiljak.helpers.NumberHelper;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class Receipt {
     private List<ReceiptItem> items;
@@ -36,5 +37,14 @@ public class Receipt {
 
     public float calculateTotalPrice(){
         return ((float) items.stream().mapToDouble(ReceiptItem::getPriceWithTax).sum());
+    }
+
+    @Override
+    public String toString() {
+        List<String> itemList = items.stream().map((item) -> "> " + item.toString()).toList();
+        String stringItems = String.join("\n", itemList);
+        return stringItems + "\n"
+                + "> Sales Taxes: " + String.format(Locale.ENGLISH, "%.02f", calculateTax()) + "\n"
+                + "> Total: " + String.format(Locale.ENGLISH, "%.02f", calculateTotalPrice());
     }
 }
